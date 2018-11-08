42fbd949a6eb3a51ba5967a3d2c6d66ee301b94a
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-11-08 07:24:06 -0500

    Added initial problems

---
+++ Homework8.java
@@ -0,0 +1,27 @@
+public class Homework8 {
+
+	public Homework8(int n) {
+
+	}
+
+	public int[] problem1() {
+
+	}
+
+	public void problem2(int n) {
+
+	}
+
+	public void problem3() {
+
+	}
+
+	public int problem4(double[] arr) {
+
+	}
+
+	public int problem5(int[] arr, int num) {
+
+	}
+	
+}
