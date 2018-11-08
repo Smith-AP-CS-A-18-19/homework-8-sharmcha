cc0315722d78b0071c80aac3b2861f1f37e92825
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-11-08 07:24:28 -0500

    Refined problems

--- Homework8.java
+++ Homework8.java
@@ -1,27 +1,44 @@
 public class Homework8 {
 
+	/* Finish the constructor and create any necessary instance
+	 * variables. A Homework8 object should have an array that
+	 * holds n int values
+	 */
 	public Homework8(int n) {
 
 	}
 
+	/* Return the stored array
+	 */
 	public int[] problem1() {
 
 	}
 
+	/* Fill the stored array with the provided int
+	 */
 	public void problem2(int n) {
 
 	}
 
-	public void problem3() {
+	/* Fill the stored array. The first value should be
+	 * n, the second n-1, the third n-2, etc.
+	 */
+	public void problem3(int n) {
 
 	}
 
+	/* Find and return the maximum value in the
+	 * provided array
+	 */
 	public int problem4(double[] arr) {
 
 	}
 
+	/* Find and return the index of num in the
+	 * provided array
+	 */
 	public int problem5(int[] arr, int num) {
 
 	}
-	
+
 }
